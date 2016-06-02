6e4e12399d6096723a53fa225ad366bb2bf72986
Steffen Allner <sa@gocept.com>
2016-06-02 14:21:09 +0200

    Fix test.

--- src/gocept/jenkinsdsl/tests/test_jenkinsdsl.py
+++ src/gocept/jenkinsdsl/tests/test_jenkinsdsl.py
@@ -68,6 +68,7 @@ redmine_project_name = gocept.jenkinsdsl
     expected_jobconfig = (
         r"new JobConfig(name: 'gocept.jenkinsdsl', "
         r"description: 'Buildout and test of the gocept.jenkinsdsl package', "
+        r"disabled: 'false', "
         r"vcs: new HG(name: 'gocept.jenkinsdsl', "
         r"baseurl: '''https://bitbucket.org''', group: '''gocept'''), "
         r"builder: new PytestBuilder(timeout: '''40''', "

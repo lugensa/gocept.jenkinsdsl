41cf4997c3a5ca57b0da2bfe149c39ba323f30e7
Michael Howitz <mh@gocept.com>
2020-05-13 17:44:30 +0200

    9

--- src/gocept/jenkinsdsl/templates/git.groovy
+++ src/gocept/jenkinsdsl/templates/git.groovy
@@ -41,7 +41,7 @@ class GIT implements VersionControlSystem {
                     // }
                     if (this.branch != null ) {
                         configure {
-                            scm -> scm / branches {
+                            project -> project / scm / branches {
                                 name this.branch
                             }
                         }
